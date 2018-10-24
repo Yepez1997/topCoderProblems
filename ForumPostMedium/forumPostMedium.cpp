26a11a6c6c7929b8c5df424331d120f05c14c3ab
Aureliano Yepez <ayepez2020@berkeley.edu>
2018-10-23 17:57:00 -0700

    more problems added

---
+++ ForumPostMedium/forumPostMedium.cpp
@@ -0,0 +1,51 @@
+/*
+* ForumPostMedium: using a 24 hour time 
+*/
+
+
+#include <iostream>
+#include <vector>
+#include <sstream>
+#include <deque>
+
+#define watch(x) cout << (#x) << " is " << (x) << '\n' 
+
+
+
+
+using namespace std;
+	
+
+std::
+
+std::deque<std::string> tokenize(std::string &any_time, std::deque<std::string> &ds){ 
+ 
+	int count_max = 0; 
+	std::stringstream ss(any_time); 
+	
+	// do parsing and add to appro
+	while (ss) { 
+	
+
+	}
+
+	  
+} 
+
+
+int main() { 
+	// two strings to test 
+	// can use hashmap to distiguihs  ? 
+	// hours,minutes,seconds
+	std::deque<std::string> current_time; 
+	std::deque<std::string> post_time;
+	std::string current_time = "12:12:12";
+	std::string post_time "12:11:13"; 
+	
+	// shoud return "few seconds ago" 
+	 
+
+	return 0; 
+} 
+
+
