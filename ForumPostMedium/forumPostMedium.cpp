50836a58460f1a9a6181045bdae8bf289a0a35d5
Aureliano Yepez <ayepez2020@berkeley.edu>
2018-10-23 18:13:27 -0700

    check for right deque values

--- ForumPostMedium/forumPostMedium.cpp
+++ ForumPostMedium/forumPostMedium.cpp
@@ -9,27 +9,35 @@
 #include <deque>
 
 #define watch(x) cout << (#x) << " is " << (x) << '\n' 
-
+#define PB push_back
 
 
 
 using namespace std;
 	
 
-std::
 
-std::deque<std::string> tokenize(std::string &any_time, std::deque<std::string> &ds){ 
+std::deque<std::string> tokenize_to_deque(std::string &any_time, std::deque<std::string> &ds){ 
  
 	int count_max = 0; 
 	std::stringstream ss(any_time); 
-	
+	std::string holder= " ";	
 	// do parsing and add to appro
-	while (ss) { 
-	
-
+	while (ss) {
+ 
+		if (ss.peek() == ':') {
+			ss.get(); 
+			ds.PB(holder); 
+			// reset holde r
+			holder = " "; 
+		}
+		else { 
+			holder ++ ss.get(); // keep getting until encounter ';'	
+		}
 	}
 
-	  
+	return ds 
+
 } 
 
 
@@ -37,11 +45,15 @@ int main() {
 	// two strings to test 
 	// can use hashmap to distiguihs  ? 
 	// hours,minutes,seconds
-	std::deque<std::string> current_time; 
-	std::deque<std::string> post_time;
+	std::deque<std::string> current_ds; 
+	std::deque<std::string> post_ds;
 	std::string current_time = "12:12:12";
-	std::string post_time "12:11:13"; 
+	std::string post_time "12:11:13";
+
+	current_ds = tokenize_to_deque(current_time); 
+	post_ds = tokenize_to_deque(post_time);
 	
+	// should we test if values are correct? 
 	// shoud return "few seconds ago" 
 	 
 
